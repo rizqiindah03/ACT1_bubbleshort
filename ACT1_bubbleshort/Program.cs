1d8d472e653babc7518ac798fd3d3b39d6af6586
rizqiindah03 <rizqiindah32@gmail.com>
2022-10-07 17:40:03 +0700

    menambahkan method bubblesort

--- ACT1_bubbleshort/Program.cs
+++ ACT1_bubbleshort/Program.cs
@@ -50,6 +50,24 @@ namespace Bubble_sort
             }
             Console.WriteLine("");
         }
+        public void BubbleSortArray()
+        {
+            for (int i = 1; i < n; i++) // For n - 1 passes
+            {
+                // Pada pass i, bandingkan n - i elemen pertama dengan elemen selanjutnya 
+                for (int j = 0; j < n - i; j++)
+                {
+                    if (a[j] > a[j + 1]) // jika elemen tidak dalam urutan yang benar
+                    {
+                        // Tukar elemen 
+                        int temp;
+                        temp = a[j];
+                        a[j] = a[j + 1];
+                        a[j + 1] = temp;
 
+                    }
+                }
+            }
+        }                            
     }
 }
